1a4f84ef4a09b857dea521eeaaeaaa9979b99a2f
Shradha Madkaikar <125337497+shradhamadkaikar@users.noreply.github.com>
2024-11-15 01:55:10 +0530

    Add files via upload

---
+++ Operators/ArithmeticOperation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo.Operators
+{
+    public class ArithmeticOperation
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Enter the num1: ");
+        //    double num1 = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.Write("Enter the num2: ");
+        //    double num2 = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.WriteLine("1- Add: "  +(num1+num2));
+        //    Console.WriteLine("2- Sub: " +(num1-num2));
+        //    Console.WriteLine("3- Mul: " +(num1*num2));
+
+        //    Console.WriteLine("4- Div: " +(num1/num2));
+        //}
+    }
+}
