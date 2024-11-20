f7838a45d00591ab7ec87db9e5f1d532d39339e7
Shradha Madkaikar <125337497+shradhamadkaikar@users.noreply.github.com>
2024-11-21 01:50:53 +0530

    Add files via upload

---
+++ SerializationAssesment/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SerializationAssesment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cityName = txtCity.Text.Trim();
+
+                if (listBox1.Items.Contains(cityName))
+                {
+                    MessageBox.Show("City already exists.");
+                    return;
+                }
+
+                listBox1.Items.Add(cityName);
+
+                string path = @"C:\Users\HP\source\repos\Employee2\cities.dat";
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(cityName);
+                }
+
+                MessageBox.Show("City added.");
+                txtCity.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listBox1.SelectedItem != null)
+                {
+                    listBox1.Items.Remove(listBox1.SelectedItem);
+                    MessageBox.Show("City removed ");
+                }
+                else
+                {
+                    MessageBox.Show("enter city.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
