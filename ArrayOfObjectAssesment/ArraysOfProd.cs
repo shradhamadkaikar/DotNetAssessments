408b9242ad78d0921a5b8d270a62e15d874b6f82
Shradha Madkaikar <125337497+shradhamadkaikar@users.noreply.github.com>
2024-11-20 17:11:58 +0530

    Add files via upload

---
+++ ArrayOfObjectAssesment/ArraysOfProd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+///Create an array of 5 products contains id,name & price & display the products
+//Display all products - using for & foreach
+//Display the products whose price is > 500 -foreach
+
+namespace FirstDemo.ArraysOfObject
+{
+    public class ArraysOfProd
+    {
+        //static void Main(string[] args)
+        //{
+        //    // array of product
+        //    int[] arr = new int[] { 1, 2, 3, 4, 5 };
+
+        //    Product[] prod = new Product[5]
+        //    {
+        //        new Product{ProdId=1,ProdName="Laptop",ProdPrice=45000},
+        //        new Product{ProdId=2,ProdName="Mobile",ProdPrice=15000},
+        //        new Product{ProdId=3,ProdName="Carpet",ProdPrice=250},
+        //        new Product{ProdId=4,ProdName="Mug",ProdPrice=150},
+        //        new Product{ProdId=5,ProdName="Table",ProdPrice=5000},
+        //    };
+
+      
+        //    Console.WriteLine("----------Display all products using foreach-------------");
+        //    foreach (Product p in prod)
+        //    {
+        //        Console.WriteLine(p);
+        //    }
+
+
+        //    Console.WriteLine("---------Display the products whose price is >500--------");
+        //    foreach (Product p in prod)
+        //    {
+        //        if(p.ProdPrice>500)
+        //        {
+        //            Console.WriteLine($"Id ={p.ProdId} , Name= {p.ProdName} , Salary={p.ProdPrice}");
+        //        }
+
+        //    }
+
+        //}
+
+    }
+}
---
+++ ArrayOfObjectAssesment/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDemo.ArraysOfObject
+{
+    public class Product
+    {
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public double ProdPrice { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id ={ProdId} , Name= {ProdName} , Salary={ProdPrice}";
+        }
+
+    }
+}
